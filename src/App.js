9816c639dff7164a3491286f63883252f51de267
gopalvaghela <vaghelagopal160@gmail.com>
2024-02-02 19:54:49 +0530

    todo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Dlist from './Dlist';
+function App() {
+  return (
+    <div className="App">
+     <Dlist></Dlist>
+    </div>
+  );
+}
+
+export default App;
